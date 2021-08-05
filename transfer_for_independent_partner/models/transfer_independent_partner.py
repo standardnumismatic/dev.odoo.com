992928c1086a1e02dab57336f06a090b252cf721
standardnumismatic <86103516+standardnumismatic@users.noreply.github.com>
2021-08-05 12:31:45 +0500

    Add files via upload

--- transfer_for_independent_partner/models/transfer_independent_partner.py
+++ transfer_for_independent_partner/models/transfer_independent_partner.py
@@ -10,7 +10,6 @@ from datetime import date
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
-
 class StockPickingInhs(models.Model):
     _inherit = 'stock.picking'
 
@@ -25,53 +24,6 @@ class StockPickingInhs(models.Model):
         else:
             self.is_internal = False
 
-    # def action_transfer_independent(self):
-    #     picking_record = self.search([])
-    #     for record in picking_record:
-    #         if record.picking_type_id.sequence_code == 'INT':
-    #             if record.date_deadlines:
-    #                 if record.state == 'done' and record.date_deadlines <= datetime.now() and record.check_internal == False:
-    #                     print('Working')
-    #                     vals = {
-    #                             'location_id': record.location_dest_id.id,
-    #                             'location_dest_id': record.location_id.id,
-    #                             'partner_id': record.partner_id.id,
-    #                             'picking_type_id': record.picking_type_id.id,
-    #                             'origin': record.name,
-    #                         }
-    #                     picking = self.env['stock.picking'].create(vals)
-    #                     for line in record.move_line_ids_without_package:
-    #                         location_record = self.env['stock.quant'].search([('product_id', '=', line.product_id.id), ('location_id', '=', line.location_dest_id.id), ('lot_id', '=', line.lot_id.id)])
-    #                         lines = {
-    #                             'picking_id': picking.id,
-    #                             'product_id': line.product_id.id,
-    #                             # 'lot_ids': [line.lot_id.id],
-    #                             'name': line.product_id.name,
-    #                             'product_uom': line.product_id.uom_id.id,
-    #                             'location_id': record.location_dest_id.id,
-    #                             'location_dest_id': record.location_id.id,
-    #                             'product_uom_qty': location_record.available_quantity,
-    #                             'quantity_done': location_record.available_quantity,
-    #                         }
-    #                         stock_move = self.env['stock.move'].create(lines)
-    #                     if picking:
-    #                         record.check_internal = True
-    #                         picking.action_confirm()
-    #                         picking.action_assign()
-    #                         picking.button_validate()
-
-                            # moves = {
-                            #     'move_id': stock_move.id,
-                            #     'product_id': line.product_id.id,
-                            #     'lot_id': line.lot_id.id,
-                            #     'location_id': record.location_id.id,
-                            #     'location_dest_id': record.location_dest_id.id,
-                            #     'product_uom_id': line.product_id.uom_id.id,
-                            #     'product_uom_qty': location_record.available_quantity,
-                            #     'qty_done': location_record.available_quantity,
-                            # }
-                            # stock_move_line_id = self.env['stock.move.line'].create(moves)
-
     def action_transfer_independent(self):
         print('working')
         picking_record = self.search([])
@@ -79,8 +31,6 @@ class StockPickingInhs(models.Model):
             if record.picking_type_id.sequence_code == 'INT':
                 if record.date_deadlines:
                     if record.state == 'done' and record.date_deadlines <= datetime.now() and record.check_internal == False:
-                        line_val = []
-                        line_val_line = []
                         rec = self.env['stock.picking'].create({
                             'check_internal': True,
                             'partner_id': record.partner_id.id,
@@ -88,56 +38,16 @@ class StockPickingInhs(models.Model):
                             'location_id': record.location_dest_id.id,
                             'location_dest_id': record.location_id.id,
                             'origin': record.name,
-                            # 'move_ids_without_package': line_val,
-                            # 'move_line_ids_without_package': line_val_line,
                         })
                         for line in record.move_line_ids_without_package:
                             location_record = self.env['stock.quant'].search([('product_id', '=', line.product_id.id), ('location_id', '=', line.location_dest_id.id), ('lot_id', '=', line.lot_id.id)])
-                            # line_val.append((0, 0, {
-                            #     'product_id': line.product_id.id,
-                            #     'name': line.product_id.name,
-                            #     # 'lot_ids': [line.lot_id.id],
-                            #     'lot_ids': line.lot_id.ids,
-                            #     'product_uom_qty': location_record.available_quantity,
-                            #     'product_uom': line.product_id.uom_id.id,
-                            #     'location_id': record.location_dest_id.id,
-                            #     'location_dest_id': record.location_id.id,
-                            #     'reserved_availability': location_record.available_quantity,
-                            #     'quantity_done': location_record.available_quantity,
-                            # }))
-                            # line_val_line.append((0, 0, {
-                            #     'product_id': line.product_id.id,
-                            #     # 'name': line.product_id.name,
-                            #     # 'lot_ids': [line.lot_id.id],
-                            #     'lot_id': line.lot_id.id,
-                            #     'product_uom_qty': location_record.available_quantity,
-                            #     'product_uom_id': line.product_id.uom_id.id,
-                            #     'location_id': record.location_dest_id.id,
-                            #     'location_dest_id': record.location_id.id,
-                            #     # 'reserved_availability': location_record.available_quantity,
-                            #     'qty_done': location_record.available_quantity,
-                            # }))
-                        # rec = self.env['stock.picking'].create({
-                        #     'check_internal': True,
-                        #     'partner_id': record.partner_id.id,
-                        #     'picking_type_id': record.picking_type_id.id,
-                        #     'location_id': record.location_dest_id.id,
-                        #     'location_dest_id': record.location_id.id,
-                        #     'origin': record.name,
-                        #     # 'move_ids_without_package': line_val,
-                        #     # 'move_line_ids_without_package': line_val_line,
-                        # })
                             line1 = self.env['stock.move.line'].create({
                                 'product_id': line.product_id.id,
-                                # 'name': line.product_id.name,
-                                # 'lot_ids': [line.lot_id.id],
                                 'lot_id': line.lot_id.id,
                                 'product_uom_qty': location_record.available_quantity,
                                 'product_uom_id': line.product_id.uom_id.id,
                                 'location_id': record.location_dest_id.id,
                                 'location_dest_id': record.location_id.id,
-                                # 'reserved_availability': location_record.available_quantity,
-                                # 'qty_done': location_record.available_quantity,
                                 'qty_done': location_record.available_quantity,
                                 'picking_id': rec.id,
                             })
@@ -148,10 +58,10 @@ class StockPickingInhs(models.Model):
                         if rec:
                             record.check_internal = True
 
+
 class stockQuantAutoUpdate(models.Model):
-    _inherit ="stock.quant" 
-    
-    
+    _inherit = 'stock.quant'
+
     @api.model
     def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, strict=False):   
         """ Increase the reserved quantity, i.e. increase `reserved_quantity` for the set of quants
@@ -167,16 +77,13 @@ class stockQuantAutoUpdate(models.Model):
         """
         self = self.sudo()
         rounding = product_id.uom_id.rounding
-        
+
         quants = self._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
-        
-        
+
         if 'button_validate_picking_ids' in self.env.context:
             pic_id = self.env['stock.picking'].search([('id','=',self.env.context['button_validate_picking_ids'][0])])
             reserve_quant= self.update_reserve_quant_forcefully(pic_id.id , quants)
-            
-            
-            
+
         reserved_quants = []
 
         if float_compare(quantity, 0, precision_rounding=rounding) > 0:
@@ -213,8 +120,7 @@ class stockQuantAutoUpdate(models.Model):
             if float_is_zero(quantity, precision_rounding=rounding) or float_is_zero(available_quantity, precision_rounding=rounding):
                 break
         return reserved_quants
-    
-    
+
     def update_reserve_quant_forcefully(self ,picking_id, quants):
         pick_rec = self.env['stock.picking'].search([('id','=', picking_id)])
         if pick_rec.move_line_ids:
